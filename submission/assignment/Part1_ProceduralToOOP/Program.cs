16cf52a300a1e9b61e4f7261f9385c2587656fad
Abdullah Bdreldin <abdullahbdreldin@gmail.com>
2026-09-21 18:19:43 +0300

    set project files

---
+++ submission/assignment/Part1_ProceduralToOOP/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Part1_ProceduralToOOP
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
